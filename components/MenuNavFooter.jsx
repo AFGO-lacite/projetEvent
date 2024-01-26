156263c2d193d3fc9ba6f80015e4a9be5be635ab
ghofranemj <ghofranemejri89@gmail.com>
2024-01-25 22:08:49 -0500

    ajout du comnposant MenuNavFooter

---
+++ components/MenuNavFooter.jsx
@@ -0,0 +1,52 @@
+import styles from "./MenuNav.module.css";
+import Image from "next/image";
+import fb from "@/public/fb.png";
+import insta from "@/public/insta.png";
+import linkedin from "@/public/link.png";
+
+export const MenuNavFooter = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul className={"m-3"}>
+        <li className={styles.mar}>
+          <ul>
+            <li className={"mt-4 "}>
+              <a href="">Contactez-nous</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <li className={" mb-4 "}>
+              <ul className={"d-flex flex-nowrap"}>
+                <li className={" "}>
+                  <a href="">
+                    <Image src={fb} alt="Icone Facebook"></Image>
+                  </a>
+                </li>
+                <li className={" "}>
+                  <a href="">
+                    <Image
+                      className={"rounded"}
+                      src={insta}
+                      alt="Icone Instagrame"
+                    ></Image>
+                  </a>
+                </li>
+                <li className={""}>
+                  <a href="">
+                    <Image
+                      className={"rounded"}
+                      src={linkedin}
+                      alt="Icone LinkedIn"
+                    ></Image>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
+};
