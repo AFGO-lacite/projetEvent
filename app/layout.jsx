c3ff8546825b3898d6e5761d7e8690b3bf10f3b2
aminefaw89 <aminefaw89@gmail.com>
2024-02-01 22:21:17 -0500

    ajout de la page oldCars et modification du layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { Inter } from "next/font/google";
 import "normalize.css/normalize.css";
 import "../style/globals.css";
@@ -5,19 +6,39 @@ import { Header } from "@/components/Header.jsx";
 import { Footer } from "@/components/Footer.jsx";
 import styles from "./layout.module.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+import Accueil from "@/components/Accueil";
+import { MotoPage } from "@/components/MotoPage";
+import { MuscleCars } from "@/components/MuscleCars";
+import { OldCars } from "@/components/OldCars";
+
 const inter = Inter({ subsets: ["latin"] });
 
+/*
 export const metadata = {
   title: "Car Tuning Show",
   description: "Event",
 };
-
+*/
 export default function RootLayout({ children }) {
+  const [page, setPage] = useState("accueil");
   return (
     <html lang="en">
       <body className={inter.className + " " + styles.body}>
-        <Header />
-        <main className={styles.main + " "}>{children}</main>
+        <Header setPage={setPage} />
+        <main className={styles.main + " "}>
+          {page === "accueil" ? (
+            <Accueil />
+          ) : page === "MotoPage" ? (
+            <MotoPage />
+          ) : page === "MuscleCars" ? (
+            <MuscleCars />
+          ) : page === "OldCars" ? (
+            <OldCars />
+          ) : (
+            <div>404</div>
+          )}
+        </main>
 
         <Footer></Footer>
       </body>
