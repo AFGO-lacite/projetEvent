09456edf7697ef7bd9a6febdbab6639d949c4167
aminefaw89 <aminefaw89@gmail.com>
2024-02-08 23:37:55 -0500

    commit finale

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,13 @@
 import styles2 from "./Footer.module.css";
 import { MenuNavFooter } from "./MenuNavFooter.jsx";
 
-export const Footer = () => {
+export const Footer = ({ setPage }) => {
   return (
     <footer className={"d-flex justify-content-between p-5"}>
       <p className={styles2.title2}>
         &copy; 2024 Car Tuning Show. Amine,Ayoub et Ghofrane{" "}
       </p>
-      <MenuNavFooter className={""}></MenuNavFooter>
+      <MenuNavFooter setPage={setPage} className={""}></MenuNavFooter>
     </footer>
   );
 };
