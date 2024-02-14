745bc288153dcdcdef7512af371b93db1bd3e1e8
aminefaw89 <aminefaw89@gmail.com>
2024-02-14 12:17:25 -0500

    fixer le probleme de simple quote

--- components/InfoPage.jsx
+++ components/InfoPage.jsx
@@ -8,7 +8,7 @@ export const Info = () => {
         <h3>CarTuningShow: Explorez le Monde du Tuning Automobile</h3>
         <p>
           CarTuningShow est le portail ultime pour tous ceux qui sont captivés
-          par l'univers du tuning automobile. Notre site web est conçu pour
+          par l`&apos;`univers du tuning automobile. Notre site web est conçu pour
           offrir une expérience complète et immersive dans le monde des car
           tuning shows.
         </p>
@@ -20,7 +20,7 @@ export const Info = () => {
           Au cœur de CarTuningShow se trouve un calendrier interactif qui
           affiche toutes les dates importantes des événements de tuning à venir.
           Les utilisateurs peuvent parcourir les événements par date, type ou
-          localisation, rendant la recherche d'un événement spécifique rapide et
+          localisation, rendant la recherche d`&apos;`un événement spécifique rapide et
           conviviale.
         </p>
       </div>
@@ -28,8 +28,8 @@ export const Info = () => {
       <div>
         <h3>Détails des Événements :</h3>
         <p>
-          Chaque événement listé sur notre site est accompagné d'une description
-          détaillée, incluant le type d'événement (comme les shows, les
+          Chaque événement listé sur notre site est accompagné d`&apos;`une description
+          détaillée, incluant le type d`&apos;`événement (comme les shows, les
           compétitions ou les meet-ups), les véhicules présentés, et les
           activités spéciales prévues. Cette section est conçue pour aider les
           visiteurs à trouver des événements qui correspondent parfaitement à
@@ -51,7 +51,7 @@ export const Info = () => {
       <div>
         <h3>Achat de Billets :</h3>
         <p>
-          Une fonctionnalité clé de CarTuningShow est la possibilité d'acheter
+          Une fonctionnalité clé de CarTuningShow est la possibilité d`&apos;`acheter
           des billets directement sur le site. Les utilisateurs peuvent
           sélectionner leurs événements, choisir leurs sièges et effectuer des
           transactions sécurisées, le tout en quelques clics.
